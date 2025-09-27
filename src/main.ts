8e1ae5f291ab46131d04858379fae7fe92ecef72
AndresSmart <119899007+AndresSmart@users.noreply.github.com>
2025-09-27 07:16:00 -0500

    Ensure numeric port configuration

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
     credentials: true,
   })
 
-  const port = process.env.PORT || 3000
+  const port = Number.parseInt(process.env.PORT ?? "3000", 10) || 3000
   await app.listen(port, "0.0.0.0")
 
   console.log(`Application is running on: http://localhost:${port}`)
